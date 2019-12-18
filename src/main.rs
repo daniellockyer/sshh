a7d7759d0afdafb273c31ff46e26b8bc534c9eb9
Daniel Lockyer <hi@daniellockyer.com>
2019-12-18 19:42:17 +0700

    Add -l to list servers

--- src/main.rs
+++ src/main.rs
@@ -63,16 +63,18 @@ fn main() {
                 .value_name("GROUP")
                 .takes_value(true),
         )
+        .arg(Arg::with_name("list").short("l").long("list"))
         .arg(
             Arg::with_name("name")
                 .help("Sets the server name")
-                .required(true)
+                .required_unless("list")
                 .index(1),
         )
         .get_matches();
 
     let config_group = matches.value_of("group");
-    let config_name = matches.value_of("name").expect("name not given");
+    let config_name = matches.value_of("name");
+    let config_list = matches.is_present("list");
 
     let config_path = if let Some(config_path) = matches.value_of("config") {
         PathBuf::from(config_path)
@@ -90,8 +92,15 @@ fn main() {
 
     if config_group.is_none() {
         for server in server_mapping.servers {
-            if server.name == config_name {
-                server_found = Some(server);
+            if config_list {
+                println!("  - {}", server.name);
+                continue;
+            }
+
+            if let Some(config_name) = config_name {
+                if config_name == server.name {
+                    server_found = Some(server);
+                }
             }
         }
     }
@@ -103,9 +112,20 @@ fn main() {
             }
         }
 
+        if config_list {
+            println!("{}:", group.name);
+        }
+
         for server in group.servers {
-            if server.name == config_name {
-                server_found = Some(server);
+            if config_list {
+                println!("  - {}", server.name);
+                continue;
+            }
+
+            if let Some(config_name) = config_name {
+                if config_name == server.name {
+                    server_found = Some(server);
+                }
             }
         }
     }
